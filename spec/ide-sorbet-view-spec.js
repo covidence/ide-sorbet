e5e47bb0abe78b0f33ded08ca303924df208c981
Andrew O'Neil <andy@andyofniall.net>
2019-07-26 10:38:59 +1000

    Initial commit

---
+++ spec/ide-sorbet-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import IdeSorbetView from '../lib/ide-sorbet-view';
+
+describe('IdeSorbetView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
